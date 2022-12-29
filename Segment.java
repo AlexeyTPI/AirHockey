6e2caea439ecf1a0897118d7af9d56d7197167b3
Oleksii Bozhko <bozhko@stud.uniza.sk>
2022-12-29 12:17:41 +0100

    commit9

---
+++ Segment.java
@@ -0,0 +1,20 @@
+private CustomRectangle customRectangle;
+
+    public Segment(int width, int height, int x, int y) {
+         this.customRectangle = new CustomRectangle();
+         this.customRectangle.changeSide(width, height);
+         this.customRectangle.moveHorizontal(x);
+         this.customRectangle.moveVertical(y);
+         this.customRectangle.show();
+        }
+
+public void turnOn() {
+        this.customRectangle.show();
+        }
+
+public void turnOff() {
+        this.customRectangle.hide();
+        }
+
+
+
