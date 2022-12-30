99ad890dabc664335c01353b0dc879e8f91052d6
Vladyslav Brovkov <brovkov777@gmail.com>
2022-12-30 12:10:10 +0200

    commit

--- Manager.java
+++ Manager.java
@@ -10,22 +10,21 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
 
 /**
- * Automaticaly sends commands to the objects:<br />
+ * Automatically sends commands to the objects:<br />
  * moveDown() - when button DOWN is pressed<br />
  * moveUp() - when button UP is pressed<br />
- * moveLeft() - when button LEFT is presed<br />
+ * moveLeft() - when button LEFT is pressed<br />
  * moveRight() - when button RIGHT is pressed<br />
  * activate() - when button SPACE or ENTER is pressed<br />
  * cancel() - when button ESC is pressed<br />
  * tik() - every 0,25 seconds<br />
- * chooseDiraction(x, y) - when the mouse button was pressed
+ * chooseDirection(x, y) - when the mouse button was pressed
  */
 public class Manager {
     private ArrayList<Object> managedObjects;
-    private ArrayList<Integer> deletedObjects;
     private long oldTick;
-    private static final long TICK_LENGTH = 250000000;
-    
+    private static final long TICK_LENGTH = 1;
+
     private class ManagerButton extends KeyAdapter {
         public void keyPressed(KeyEvent event) {
             if (event.getKeyCode() == KeyEvent.VK_DOWN) {
@@ -43,17 +42,17 @@ public class Manager {
             }
         }
     }
-    
+
     private class ManagerTimer implements ActionListener {
         public void actionPerformed(ActionEvent event) {
             long newTick = System.nanoTime();
             if (newTick - Manager.this.oldTick >= Manager.TICK_LENGTH || newTick < Manager.TICK_LENGTH) {
                 Manager.this.oldTick = (newTick / Manager.TICK_LENGTH) * Manager.TICK_LENGTH;
-                Manager.this.sendCommand("tik");
+                Manager.this.sendCommand("tick");
             }
         }
     }
-    
+
     private class ManagerHasTo extends MouseAdapter {
         public void mouseClicked(MouseEvent event) {
             if (event.getButton() == MouseEvent.BUTTON1) {
@@ -61,84 +60,53 @@ public class Manager {
             }
         }
     }
-    
+
     private void sendCommand(String selector) {
-        for (Object adresat : this.managedObjects) {
+        for (Object receiver : this.managedObjects) {
             try {
-                    if (adresat != null) {                    
-                    Method command = adresat.getClass().getMethod(selector); //знаходжу метод у класі
-                    command.invoke(adresat); //викликаю метод
+                if (receiver != null) {
+                    Method command = receiver.getClass().getMethod(selector);
+                    command.invoke(receiver);
                 }
             } catch (SecurityException | NoSuchMethodException |
-                IllegalArgumentException| IllegalAccessException | InvocationTargetException e) { //перевірка та обробка виключень
+                     IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
                 this.doNothing();
-            } 
+            }
         }
-        this.removeDeletedObjects();
     }
-    
+
     private void sendCommand(String selector, int firstParameter, int secondParameter) {
-        for (Object adresat : this.managedObjects) {
+        for (Object receiver : this.managedObjects) {
             try {
-                if (adresat != null) {
-                    Method command = adresat.getClass().getMethod(selector, Integer.TYPE, Integer.TYPE);
-                    command.invoke(adresat, firstParameter, secondParameter);
+                if (receiver != null) {
+                    Method command = receiver.getClass().getMethod(selector, Integer.TYPE, Integer.TYPE);
+                    command.invoke(receiver, firstParameter, secondParameter);
                 }
-            } catch (SecurityException e) {
-                this.doNothing();
-            } catch (NoSuchMethodException e) {
-                this.doNothing();
-            } catch (IllegalArgumentException e) {
-                this.doNothing();
-            } catch (IllegalAccessException e) {
-                this.doNothing();
-            } catch (InvocationTargetException e) {
+            } catch (SecurityException | NoSuchMethodException | IllegalArgumentException | IllegalAccessException |
+                     InvocationTargetException e) {
                 this.doNothing();
             }
         }
-        this.removeDeletedObjects();
     }
-    
+
     private void doNothing() {
-        
     }
-    
-    private void removeDeletedObjects() {
-        if (this.deletedObjects.size() > 0) {
-            Collections.sort(this.deletedObjects, Collections.reverseOrder());
-            for (int i = this.deletedObjects.size() - 1; i >= 0; i--) {
-                this.managedObjects.remove(this.deletedObjects.get(i));
-            }
-            this.deletedObjects.clear();
-        }        
-    }
-    
+
     /**
      * Create new manager, if it doesn't exist.
      */
     public Manager() {
-        this.managedObjects= new ArrayList<>();
-        this.deletedObjects = new ArrayList<>();
+        this.managedObjects = new ArrayList<>();
         Polygon.getPolygon().addKeyListener(new ManagerButton());
         Polygon.getPolygon().addTimerListener(new ManagerTimer());
         Polygon.getPolygon().addMouseListener(new ManagerHasTo());
     }
-    
+
     /**
-     * Manager sends commands to the sertain object.
+     * Manager sends commands to the certain object.
      */
     public void manageObjects(Object objects) {
         this.managedObjects.add(objects);
     }
-    
-    /**
-     * Manager sends commands to the sertain object.
-     */
-    public void stopManagingObject(Object object) {
-        int index = this.managedObjects.indexOf(object);
-        if (index >= 0) {
-            this.managedObjects.set(index, null);
-            this.deletedObjects.add(index);
-        }
-    }
+
 }
